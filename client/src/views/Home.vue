7af5b035a26a5d887fc2b7f188da38a14ed53ee9
Nguyen Manh Tuan Anh <xdorro@gmail.com>
2020-08-26 16:23:12 +0700

    Update client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<style scoped>
+.home {
+    background-color: #fff;
+}
+</style>
+
+<script>
+// @ is an alias to /src
+export default {
+    name: "Home",
+    components: {
+        HelloWorld: () =>
+            import(
+                /* webpackChunkName: "helloworld" */ "@/components/HelloWorld.vue"
+            ),
+    },
+};
+</script>
